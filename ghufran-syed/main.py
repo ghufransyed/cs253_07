e7f5429b030a149e673685c8a9128335d360884c
Ghufran Syed <ghufran.syed@gmail.com>
2014-04-02 15:26:21 -0400

    add 'official' regexp from instructor notes to main.py

--- ghufran-syed/main.py
+++ ghufran-syed/main.py
@@ -2,26 +2,16 @@ import webapp2
 from signup import SignupHandler
 from login import LoginHandler
 from logout import LogoutHandler
-from formpage import FormPage  # change names and ref to data models
 from wikipage import WikiPage
-# from welcome import WelcomeHandler
-# from mainpage import MainPage
-# from mainpagejson import MainPageJson
-# from permalinkjson import PermalinkJson
-# from flushhandler import FlushHandler
 
+
+PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 
 app = webapp2.WSGIApplication([
     (r'/signup', SignupHandler),
     (r'/login', LoginHandler),
     (r'/logout', LogoutHandler),
-    (r'/(.*)', WikiPage),  # need to change to take strings
-    # TODO change WikiPage to test if exists, go to edit page
-    # if not , else to retrieve page
-    (r'/newpost', FormPage),
-    # (r'/', MainPage),
-    # (r'/.json', MainPageJson),
-    # (r'/(\d+).json', PermalinkJson),
-    # (r'/flush', FlushHandler),
-    # (r'/welcome', WelcomeHandler), not needed for wiki
+    (r'/_edit' + PAGE_RE, EditPage),
+    (r'/_history' + PAGE_RE, HistoryPage),
+    (PAGE_RE, WikiPage),
 ], debug=True)
