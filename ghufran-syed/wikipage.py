735020201766db761719b36d21658870e6ed1625
Ghufran Syed <ghufran.syed@gmail.com>
2014-04-02 15:36:08 -0400

    delete regexp address handling from wikipage.py
    As this is now handled by main.py
    Insert redirect to _edit page

--- ghufran-syed/wikipage.py
+++ ghufran-syed/wikipage.py
@@ -2,25 +2,10 @@ from mainhandler import Handler, params
 from models import WikiData
 from google.appengine.api import memcache
 import logging
-import re
 
 
 class WikiPage(Handler):
     def get(self, wikipage_address):
-        self.write((repr(wikipage_address)))
-        self.write("\n")
-
-        test = re.search("(\w+)", wikipage_address)
-        if test:
-            logging.error("test positive")
-            wikipage_address = test.group(0)
-            self.write(wikipage_address)
-        else:
-            logging.error("test negative")
-            wikipage_address = "/"
-            print "wikipage_address is '{0}'".format(wikipage_address)
-            self.write(wikipage_address)
-
         wikipage_query = memcache.get(wikipage_address)
         if wikipage_query is not None:
             logging.error('cache-hit')
@@ -35,5 +20,4 @@ class WikiPage(Handler):
                 params["wikipage_query"] = wikipage_query
                 self.render("wikipage.html", **params)
             else:
-                logging.error("wikipage_query empty, need redirect to edit page")
-                pass  # TODO  redirect edit page
+                self.redirect("_edit" + wikipage_address)
